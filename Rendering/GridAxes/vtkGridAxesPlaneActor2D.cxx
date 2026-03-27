McrmDev/VTK

commit b24c6b0e2b5046cb4b79d6fe7db2dce229a0f34a

vtkAbstractArray: Deprecate Allocate, Add ReserveValues

Rendering/GridAxes/vtkGridAxesPlaneActor2D.cxx
@@ -180,7 +180,7 @@ void vtkGridAxesPlaneActor2D::Update(vtkViewport* viewport)
     return;
   }
 
-  this->PolyDataPoints->Allocate(this->LineSegments.size() * 2);
+  this->PolyDataPoints->Reserve(this->LineSegments.size() * 2);
   this->PolyDataPoints->SetDataType(VTK_FLOAT);
   this->PolyDataLines->AllocateEstimate(static_cast<vtkIdType>(this->LineSegments.size()), 2);
   for (std::deque<LineSegmentType>::iterator iter = this->LineSegments.begin(),

commit bf49bab4575dbd8ceeb2dae564744fe6c4238ee8

vtkGridAxesPlaneActor2D: Fix allocation bug
Introduced by b24c6b0e2b5046cb4b79d6fe7db2dce229a0f34a

Rendering/GridAxes/vtkGridAxesPlaneActor2D.cxx
@@ -180,8 +180,9 @@ void vtkGridAxesPlaneActor2D::Update(vtkViewport* viewport)
     return;
   }
 
-  this->PolyDataPoints->Reserve(this->LineSegments.size() * 2);
   this->PolyDataPoints->SetDataType(VTK_FLOAT);
+  this->PolyDataPoints->Reset();
+  this->PolyDataPoints->Reserve(this->LineSegments.size() * 2);
   this->PolyDataLines->AllocateEstimate(static_cast<vtkIdType>(this->LineSegments.size()), 2);
   for (std::deque<LineSegmentType>::iterator iter = this->LineSegments.begin(),
                                              max = this->LineSegments.end();